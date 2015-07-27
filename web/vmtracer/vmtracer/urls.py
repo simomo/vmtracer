fe380ffcb854a3c0b4d5b1fc715c7b36a30848a6
simomo <qihangzp@gmail.com>
2015-07-27 23:28:48 +0000

    ....

---
+++ web/vmtracer/vmtracer/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from main import views as main_views
+from vmtracer import settings
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'vmtracer.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>(?:js|css|img)/.*)$', 'django.contrib.staticfiles.views.serve', kwargs={'document_root': settings.STATIC_ROOT}),
+    url(r'^$', 'django.contrib.staticfiles.views.serve', kwargs={
+                'path': '/static/index.html'}),
+)
