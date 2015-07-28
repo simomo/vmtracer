21435c648ad4928a14ec5d411ba546e161ec127e
DuoZhang <duozhang@vmware.com>
2015-07-28 21:44:44 +0800

    ...

--- web/vmtracer/static/js/main.js
+++ web/vmtracer/static/js/main.js
@@ -9,15 +9,24 @@ myApp
     self.timelineData = function () {
         var timelineData = new vis.DataSet();
         if (self.rawData.ops_info) {
-            
+            var rawDataLen = self.rawData.ops_info.length;
+            for (var i=0; i<rawDataLen; i++) {
+                timelineData.add({
+                    'start': self.rawData.ops_info[i].starttime,
+                    'end': self.rawData.ops_info[i].endtime,
+                    'content': self.rawData.ops_info[i].vmoptname
+                });
+            };
         }
+        return timelineData;
     };
 })
 .run(function ($http, initData) {
-    $http.get('/init')
-        .success(function (data, status, headers, config) {
-            initData.rawData = data;
-        });
+    // $http.get('/init')
+    //     .success(function (data, status, headers, config) {
+    //         initData.rawData = data;
+    //     });
+    initData.rawData = serverData;
 })
 .controller('appRoot', ['$scope', '$http', 'initData', function ($scope, $http, initData) {
     $scope.initData = initData;
@@ -28,5 +37,9 @@ myApp
 .directive('timeline', function () {
     return function (scope, element, attrs) {
         console.log(scope.initData);
+        var timelineData = scope.initData.timelineData()
+        if (timelineData) {
+            var timeline = new vis.Timeline(element[0], timelineData, {});
+        }
     }
 })
